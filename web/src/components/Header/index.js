d82a78c48295c9530e959fdd810f1060a12d2583
wen <wenshaoyou@guorou.tv>
2022-07-27 23:55:16 +0800

    feat: 路由激活导航菜单

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import classnames from 'classnames';
+import { Link, useLocation, matchPath, } from "react-router-dom";
 import useStyles from 'isomorphic-style-loader/useStyles';
 import style from './index.scss';
 import { getCls } from '@utils/style';
@@ -10,24 +11,43 @@ const Header = props => {
     {
       title: '首页',
       to: '/',
+      activePath: '/',
     },
     {
       title: '关于我们',
       to: '/company',
+      activePath: '/company',
     },
     {
       title: '产品展示',
       to: '/product',
+      activePath: '/product'
     }
   ];
+  const location = useLocation();
+  console.log('location', location)
   return (
     <div className={getCls(style, "header")}>
       <div className={getCls(style, "tab-list")}>
-        {tabs.map(tab => (
-          <Link className={getCls(style, "tab-item")} key={tab.to} to={tab.to}>
-            {tab.title}
-          </Link>
-        ))}
+        {tabs.map(tab => {
+          const routeMatch = matchPath({
+            path: tab.activePath,
+            exact: false,
+            strict: true
+          }, location.pathname);
+          // console.log('routeMatch:', routeMatch);
+          return (
+            <Link 
+              className={classnames(getCls(style, "tab-item"), {
+                [style['active']]: !!routeMatch,
+              })} 
+              key={tab.to}
+              to={tab.to}
+            >
+              {tab.title}
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
